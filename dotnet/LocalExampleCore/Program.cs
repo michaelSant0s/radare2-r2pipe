6357244b75f70b8277840ead2141155100d1a062
max <email@maxlutz.de>
2019-12-05 18:03:05 +0100

    ADD core example

---
+++ dotnet/LocalExampleCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+using r2pipe;
+
+namespace LocalExampleCore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
